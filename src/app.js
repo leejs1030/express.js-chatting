590e238c479ea75b68f377d3cacd6e31e7506943
leejs1030 <64299554+leejs1030@users.noreply.github.com>
2022-01-23 20:46:43 +0900

    Make rate limit (#29)
    * Build: Install express-rate-limit
    
    * Feat: Add RateLimit to /src/aap.js
    
    -Prevent attacker make server spend time for useless request to much.
    
    close #23
    
    * Fix: Move limiter to sign-in
    
    -Limiter at app.js caused limit to normal users. So I moved it to
    sign-in. It can prevent trying to much sign in to hack password.
    -I will also add it to app.js later. Need to think about appropriate
    times.
    
    * Refact: Seperate middleware from /src/app.js (#28)

--- src/app.js
+++ src/app.js
@@ -3,26 +3,16 @@ const { errorHandler } = require('./lib/error-handler');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const session = require('express-session');
 const http = require('http');
 const server = http.createServer(app);
 const io = new require("socket.io")(server);
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME, MODE, SESSION_SECRET } = process.env;
 const csrf = require('csurf');
 const cookieParser = require('cookie-parser');
-const connectPgSimple = require('connect-pg-simple');
-let PostgreSqlStore = require('connect-pg-simple')(session);
+const { keepSignIn, sessionmiddleware, redirecter } = require('./middleware');
 
-const sessionmiddleware = session({
-	secret: SESSION_SECRET,
-	resave: false,
-	saveUninitialized: true,
-	store : new PostgreSqlStore({
-        conString: `postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
-      }),
-	cookie: {maxAge: null},
-	resave: false,
-});
+
+
 
 
 app.set('strict routing', true); // 왜? 제대로 동작하지 않음.
@@ -31,40 +21,21 @@ app.set('views', `${__dirname}/../views`);
 app.set('view engine', 'pug');
 app.set('socketio', io);
 app.set('server', server);
+
+
 app.use('/scripts', express.static(`${__dirname}/../public/scripts`));
 app.use('/styles', express.static(`${__dirname}/../public/styles`));
-app.use((req, res, next) => { // 수동적으로 strict하게 redirect
-	if (req.path.substr(-1) === '/' && req.path.length > 1) {
-	  const query = req.url.slice(req.path.length);
-	  const safepath = req.path.slice(0, -1).replace(/\/+/g, '/');
-	  console.log(query);
-	  console.log(safepath);
-	  res.redirect(301, safepath + query);
-	}
-	else {
-	  next();
-	}
-});
+
+app.use(redirecter);
 app.use(morgan(MODE !== 'prod' ? 'dev' : 'combined'));
 app.use(express.urlencoded({ extended: false }));
-
-
-
 app.use(cookieParser());
 app.use(csrf({cookie: true}));
-app.use(sessionmiddleware);
-const YEAR = 365 * 24 * 60 * 60 * 1000;
-app.use(function (req, res, next) {
-	if(req.session.keepSignedIn){
-			req.session.resave = true;
-			req.session.cookie.expires = new Date(Date.now() + YEAR);
-			req.session.cookie.maxAge = YEAR;
-	}
-	next();
-});
+app.use(sessionmiddleware(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, SESSION_SECRET));
+app.use(keepSignIn);
 app.use('/', controller);
 app.use(errorHandler);
   
 
 
-module.exports = {server, io, app, sessionmiddleware};
+module.exports = {server, io, app};
--- src/controller/auth/index.js
+++ src/controller/auth/index.js
@@ -3,8 +3,16 @@ const ctrl = require('./ctrl');
 const router = Router({
     caseSensitive: true,
 });
+const RateLimit = require('express-rate-limit');
+
+
+const limiter = (t, n) => RateLimit({
+	windowMs: t * 1000, // t 초에
+	max: n // n회까지 로그인 시도 허용
+});
+
 router.get('/sign-in', ctrl.signInForm);
-router.post('/sign-in', ctrl.signIn);
+router.post('/sign-in', limiter(60 * 60, 10), ctrl.signIn);
 
 router.get('/sign-up', ctrl.signUpForm);
 router.post('/sign-up', ctrl.signUp);

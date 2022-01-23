0755ab204223e37163563be359d51ef2e0ecdc0c
leejs1030 <leejs1030@korea.ac.kr>
2022-01-23 16:23:29 +0900

    Refact: Update /src/controller/channels/index.js
    -Comment out unused code

--- src/controller/channels/index.js
+++ src/controller/channels/index.js
@@ -10,12 +10,12 @@ const { membershipRequired, ownRequired } = require('./middleware');
 router.get('/', authRequired, ctrl.indexPage);
 router.post('/', authRequired, ctrl.createChannel);
 router.get('/:channelId', authRequired, membershipRequired, ctrl.showChannel);
-router.post('/:channelId', authRequired, membershipRequired, ctrl.sendMsg);
+// router.post('/:channelId', authRequired, membershipRequired, ctrl.sendMsg);
 router.get('/:channelId/quit', authRequired, membershipRequired, ctrl.quitChannel); // put 사용하기.
 router.get('/:channelId/delete', authRequired, membershipRequired, ownRequired, ctrl.deleteChannel); // delete 사용하기.
 
 router.get('/:channelId/invitelist', authRequired, membershipRequired, ctrl.inviteFriend);
-router.get('/:channelId/invitelist/:targetId', authRequired, membershipRequired, ctrl.includeToChannel); // post 사용하기.
+// router.get('/:channelId/invitelist/:targetId', authRequired, membershipRequired, ctrl.includeToChannel); // put 사용하기.
 
 router.get('/:channelId/members', authRequired, membershipRequired, ctrl.memberList);
 

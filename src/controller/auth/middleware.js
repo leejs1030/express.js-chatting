2604613a1c1b3bfa1b0a40b5e368c205b697ac4b
leejs1030 <leejs1030@korea.ac.kr>
2022-01-24 20:35:51 +0900

    Feat: Update Middleware.js to send response code

--- src/controller/auth/middleware.js
+++ src/controller/auth/middleware.js
@@ -1,8 +1,7 @@
 const authRequired = async (req, res, next) => {
     try {
         if (req.session.user) return next();
-        else
-            return res.redirect('/auth/sign-in');
+        else return res.status(302).redirect('/auth/sign-in');
     } catch (err) {
         return next(err);
     }
--- src/controller/channels/middleware.js
+++ src/controller/channels/middleware.js
@@ -5,13 +5,11 @@ const doesChannelExist = async(req, res, next) =>{
     try{
         const {channelId} = req.params;
         const result = await ChannelDAO.getChannelInfoById(channelId);
-        if(result instanceof Error){
-            return res.send(getAlertScript('존재하지 않는 채널입니다!'));
-        } else{
-            return next();
-        }
+        if(result instanceof Error) return res.status(404).send(getAlertScript('존재하지 않는 채널입니다!'));
+        else return next();
+        
     } catch(err){
-        console.log(err);
+        return console.log(err);
     }
 }
 
@@ -20,11 +18,8 @@ const membershipRequired = async(req, res, next) =>{
         const {channelId} = req.params;
         const {user} = req.session;
         const result = await ChannelDAO.isChannelMember(channelId, user.id);
-        if(result){
-            return next();
-        } else {
-            return res.send(getAlertScript('속하지 않은 채널은 볼 수 없습니다!'));
-        }
+        if(result) return next();
+        else return res.status(401).send(getAlertScript('속하지 않은 채널입니다!'));
     } catch(err){
         return console.log(err);
     }
@@ -35,12 +30,8 @@ const ownRequired = async(req, res, next) =>{
         const {channelId} = req.params;
         const {user} = req.session;
         const result = await ChannelDAO.isChannelCreater(channelId, user.id);
-        if(result){
-            // return next();
-            next();
-        } else {
-            return res.send(getAlertScript('관리자만이 채널을 삭제할 수 있습니다!'));
-        }
+        if(result) return next();
+        else return res.status(401).send(getAlertScript('관리자만이 접근할 수 있습니다!'));
     } catch(err){
         return console.log(err);
     }

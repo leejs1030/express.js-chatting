58d264c7112a3fa522f171d987601bfaf7ecfa6f
leejs1030 <leejs1030@korea.ac.kr>
2022-01-25 02:25:55 +0900

    Refact: move code from index.js to middleware.js
    -Move middleware function to middleware.js

--- src/controller/channels/index.js
+++ src/controller/channels/index.js
@@ -4,30 +4,27 @@ const router = Router({
 });
 const ctrl = require('./ctrl');
 const { authRequired } = require('../auth/middleware');
-const { doesChannelExist, membershipRequired, ownRequired, } = require('./middleware');
-const { getAlertScript } = require('../../lib/usefulJS');
-const digits = new RegExp('^[0-9]+$')
+const { doesChannelExist, membershipRequired, ownRequired, isChannelId, } = require('./middleware');
 
 
-router.param('channelId', (req, res, next, id) =>{
-    if(digits.exec(id)){
-        next();
-    }else{
-        res.send(getAlertScript('적절하지 않은 채널 id입니다! 자연수로 입력해주세요.'))
-    }
-});
+router.param('channelId', isChannelId);
 
 router.get('/', authRequired, ctrl.indexPage);
 router.post('/', authRequired, ctrl.createChannel);
 router.get('/:channelId', authRequired, doesChannelExist, membershipRequired, ctrl.showChannel);
-// router.post('/:channelId', authRequired, membershipRequired, ctrl.sendMsg);
+
 router.put('/:channelId', authRequired, doesChannelExist, membershipRequired, ctrl.quitChannel); // put 사용하기.
 router.delete('/:channelId', authRequired, doesChannelExist, membershipRequired, ownRequired, ctrl.deleteChannel); // delete 사용하기.
 
 router.get('/:channelId/invitelist', authRequired, doesChannelExist, membershipRequired, ctrl.inviteFriend);
-// router.get('/:channelId/invitelist/:targetId', authRequired, membershipRequired, ctrl.includeToChannel); // put 사용하기.
 
 router.get('/:channelId/members', authRequired, doesChannelExist, membershipRequired, ctrl.memberList);
 
+
+
+// router.post('/:channelId', authRequired, membershipRequired, ctrl.sendMsg); socket 사용 중.
+// 현재는 미 사용.
+// router.get('/:channelId/invitelist/:targetId', authRequired, membershipRequired, ctrl.includeToChannel); socket 사용 중.
+// 현재는 미 사용. 필요하다면 put(혹은 patch?) 사용할 것.
 
 module.exports = router;

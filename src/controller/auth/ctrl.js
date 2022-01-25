00eb05b86679366bb01e409d18cc92467d8be410
leejs1030 <leejs1030@korea.ac.kr>
2022-01-25 16:08:28 +0900

    Refact: Update ctrl.js

--- src/controller/auth/ctrl.js
+++ src/controller/auth/ctrl.js
@@ -57,9 +57,7 @@ const signUp = async (req, res, next) =>{
         const {id, password, nick} = req.body;
         if(!id || id > 20 || !password || !nick || nick > 20) throw new Error('BAD_REQUEST');
         const isExist = await UserDAO.getById(id);
-        if(isExist) {
-            return res.status(400).send(getAlertScript("이미 존재하는 ID입니다!"));
-        }
+        if(isExist) return res.status(400).send(getAlertScript("이미 존재하는 ID입니다!"));
         const encryptedPassword = await generatePassword(password);
         await UserDAO.create(id, encryptedPassword, nick);
 

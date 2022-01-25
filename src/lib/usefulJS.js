568d9067ffee252a45acfbd1826ffc0657721186
leejs1030 <leejs1030@korea.ac.kr>
2022-01-26 03:10:46 +0900

    Chore: Update usefulJS.js

--- src/lib/usefulJS.js
+++ src/lib/usefulJS.js
@@ -2,7 +2,7 @@ const getAlertScript = (msg) => `<script>alert("${msg}");history.back();</script
 const errorAt = (name, err) => {
     console.log("Error at " + name);
     console.log(err);
-    throw err;
+    // throw err;
     return err;
 };
 const isNumber = (str) =>{

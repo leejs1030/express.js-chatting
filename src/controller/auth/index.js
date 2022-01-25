3c435740fc52d4b929b73a0ba580cd22edb68911
leejs1030 <leejs1030@korea.ac.kr>
2022-01-25 17:15:49 +0900

    Refact: Move code from index.js to middleware.js

--- src/controller/auth/index.js
+++ src/controller/auth/index.js
@@ -1,15 +1,11 @@
 const {Router} = require('express');
 const ctrl = require('./ctrl');
+const { limiter } = require('./middleware');
 const router = Router({
     caseSensitive: true,
 });
-const RateLimit = require('express-rate-limit');
 
 
-const limiter = (t, n) => RateLimit({
-	windowMs: t * 1000, // t 초에
-	max: n // n회까지 로그인 시도 허용
-});
 
 router.get('/sign-in', ctrl.signInForm);
 router.post('/sign-in', limiter(60 * 60, 10), ctrl.signIn);
--- src/controller/auth/middleware.js
+++ src/controller/auth/middleware.js
@@ -7,5 +7,10 @@ const authRequired = async (req, res, next) => {
     }
 };
 
+const RateLimit = require('express-rate-limit');
+const limiter = (t, n) => RateLimit({
+	windowMs: t * 1000, // t 초에
+	max: n // n회까지 로그인 시도 허용
+});
 
-module.exports = { authRequired };
+module.exports = { authRequired, limiter };

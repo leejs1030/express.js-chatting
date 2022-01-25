093d1104bbad558c41ea4091e9cbbaa7d121d082
leejs1030 <leejs1030@korea.ac.kr>
2022-01-26 03:12:06 +0900

    Chore: Update socketcontrol.js
    -Add try-catch

--- src/lib/socketcontrol.js
+++ src/lib/socketcontrol.js
@@ -2,39 +2,53 @@ const {isNumber} = require('../lib/usefulJS');
 const {ChannelDAO, SocialDAO} = require('../DAO');
 
 const initialJoinRoom = async (socket, roomnum) =>{
-	if(isNumber(roomnum)) socket.join(roomnum);
-	else{
-		const temp = socket.request.session;
-		socket.join(temp.user.id);
-		const clist = (await ChannelDAO.getChannelsByUserId(temp.user.id));
-		clist.forEach((e) => {
-			socket.join(String(e.id));
-		});
-	}
+    try{
+        if(isNumber(roomnum)) socket.join(roomnum);
+        else{
+            const temp = socket.request.session;
+            socket.join(temp.user.id);
+            const clist = (await ChannelDAO.getChannelsByUserId(temp.user.id));
+            clist.forEach((e) => {
+                socket.join(String(e.id));
+            });
+        }
+    } catch(err){
+        console.log(err);
+        return err;
+    }
 }
 
 const receiveAndSend = async (io, receiveData, roomnum) =>{
-    const receiveTime = await ChannelDAO.sendMsg(receiveData.id, roomnum, receiveData.msg);
-    const sendData = {
-        id: receiveData.id,
-        nick: receiveData.nick,
-        channel: roomnum,
-        msg: receiveData.msg,
-        msg_date: receiveTime
-    };
-    return io.to(sendData.channel).emit(`update`, sendData);
+    try{
+        const receiveTime = await ChannelDAO.sendMsg(receiveData.id, roomnum, receiveData.msg);
+        const sendData = {
+            id: receiveData.id,
+            nick: receiveData.nick,
+            channel: roomnum,
+            msg: receiveData.msg,
+            msg_date: receiveTime
+        };
+        return io.to(sendData.channel).emit(`update`, sendData);
+    } catch(err){
+        console.log(err);
+        return err;
+    }
 }
 
 const inviteFriend = async (io, socket, roomnum, targetId) =>{
-    // 변경 필요
-    await SocialDAO.includeToChannel(roomnum, targetId);
-    const channelInfo = (await ChannelDAO.getChannelInfoById(roomnum, targetId));
-    io.to(targetId).emit(`invite`, {
-        cid: roomnum,
-        cname: channelInfo.name,
-        cunread: channelInfo.unread,
-        ctime: channelInfo.updatetime,
-    });
+    try{
+        await SocialDAO.includeToChannel(roomnum, targetId);
+        const channelInfo = (await ChannelDAO.getChannelInfoById(roomnum, targetId));
+        io.to(targetId).emit(`invite`, {
+            cid: roomnum,
+            cname: channelInfo.name,
+            cunread: channelInfo.unread,
+            ctime: channelInfo.updatetime,
+        });
+    } catch(err){
+        console.log(err);
+        return err;
+    }
 }
 
 module.exports = {

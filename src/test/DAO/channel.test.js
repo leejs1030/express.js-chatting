60c8dabbf2b0aafe870b9a7207dd2d56ef1a645c
leejs1030 <leejs1030@korea.ac.kr>
2022-02-23 17:54:28 +0900

    Test: Add test code for ChannelDAO
    - Test getChannelsByUserId, getChannelUnreadById, getChannelInfoById
    - Test countChannelsByUserId

--- src/test/DAO/channel.test.js
+++ src/test/DAO/channel.test.js
@@ -1,6 +1,32 @@
 const {ChannelDAO} = require('../../main/DAO');
 const expect = require('expect.js');
+const { getChannelInfoById } = require('../../main/DAO/channel');
 
 describe('Test ChannelDAO', async () => {
-    
+    it('Test getting Channel Info related to user', async () =>{
+        let uid = 'subadmin'
+        const channels = await ChannelDAO.getChannelsByUserId(uid);
+        expect(channels).to.be.a(typeof([]));
+        for await (const e of channels) {
+            expect(e.id).a(typeof(1));
+            expect(e.id).greaterThan(0);
+            expect(e.name).a(typeof('s'));
+            expect(e.unread).a(typeof(1));
+            expect(e.unread).greaterThan(-1);
+            expect(e.updatetime).a(typeof('s'));
+            expect(e.creater).a('string');
+            let a = await ChannelDAO.getChannelUnreadById(e.id, uid);
+            expect(a).equal(e.unread);
+            let b = await ChannelDAO.getChannelInfoById(e.id, uid);
+            expect(e).eql(b);
+            let c = await ChannelDAO.getChannelInfoById(e.id);
+            expect(c.unread).equal(undefined);
+        }
+    });
+
+    it('Test count channel joined', async () =>{
+        let num = await ChannelDAO.countChannelsByUserId('subadmin');
+        expect(num).a(typeof(1));
+        expect(num).within(0, Infinity);
+    })
 });

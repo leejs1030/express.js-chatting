ca79c35ca0d57c68de2b58bc11a9c1bd675301c7
leejs1030 <leejs1030@korea.ac.kr>
2022-02-26 15:19:43 +0900

    Test: Enhance test cases

--- src/test/DAO/user.test.js
+++ src/test/DAO/user.test.js
@@ -17,6 +17,8 @@ describe('Test UserDAO', async ()=>{
         try{
             let success = await UserDAO.createUser(tempuser.id, tempuser.pass, tempuser.nick);
             expect(success).to.be(true);
+            success = await UserDAO.createUser(tempuser.id, tempuser.pass, tempuser.nick);
+            expect(success).to.be(false);
         } finally{
             await db.none('DELETE FROM users WHERE id = $1', tempuser.id);
             expect(await UserDAO.getById(tempuser.id)).equal(undefined);

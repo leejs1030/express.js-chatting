d5131b1a22acd687191df6b7e78014f1159c1921
leejs1030 <leejs1030@korea.ac.kr>
2022-02-28 00:45:43 +0900

    Test: Update test code to typescript

---
+++ src/test/DAO/user.test.ts
@@ -0,0 +1,36 @@
+import { UserDAO } from '../../main/DAO';
+import { verifyPassword } from '../../main/lib/passwords';
+import db = require('../../main/lib/dbconnection');
+import { expect } from 'chai';
+
+describe('Test UserDAO', async ()=>{
+    it('Find/Create user by using id', async ()=>{
+        
+        const admin = {id: 'admin', nick: '관리자', pass: 'admin'};
+        const user = await UserDAO.getById(admin.id);
+        expect(user.nick).equal(admin.nick);
+        expect(user.id).equal(admin.id);
+        expect(await verifyPassword(admin.pass, user.password)).to.be.true;
+        
+        let success;
+        const tempuser = {id: 'adsfjoi', nick: 'sadfi', pass: 'fsoidj'};
+        try{
+            success = await UserDAO.createUser(tempuser.id, tempuser.pass, tempuser.nick);
+            expect(success).to.be.true;
+            success = await UserDAO.createUser(tempuser.id, tempuser.pass, tempuser.nick);
+            expect(success).to.be.false;
+        } finally{
+            await db.none('DELETE FROM users WHERE id = $1', tempuser.id);
+            expect(await UserDAO.getById(tempuser.id)).equal(null);
+        }
+
+        success = await UserDAO.createUser(admin.id, admin.pass, admin.nick);
+        expect(success).equal(false);
+        
+    });
+
+    it('Set/Get uesr_settings by user id', async () =>{
+        const setting = await UserDAO.getSettingById('admin');
+        expect(setting.send_enter).to.be.a(typeof(true));
+    });
+});

8bbb815328d0b58994a9f6a21418cc765473bc0b
leejs1030 <leejs1030@korea.ac.kr>
2022-02-27 16:00:09 +0900

    Feat: Add compressWithTask/compressWithTx to DAO
    - Task is useful for chaining queries. So DAO function is now all using
    task or tx for chaining queries. But, someties controller call multiple
    DAO function. This make chaining queries doesn't work at one task, cause
    multiple connect-disconnect works, which are expensive, cause
    low performance.
    - To resolve this, add compressIntoTask and compressIntoTx functions.
    - This can be used in DAO, and help multiple DAO function calls works
    with only one connect-disconnect work. This is similar to previous
    commits, "Use pg-promise effectively". This complete the subject.

--- src/main/DAO/index.js
+++ src/main/DAO/index.js
@@ -1,8 +1,39 @@
 const UserDAO = require('./user');
 const SocialDAO = require('./social');
 const ChannelDAO = require('./channel');
+const db = require('../lib/dbconnection');
 
-module.exports = { UserDAO, SocialDAO, ChannelDAO };
+/**
+ * 
+ * @param {function} f 
+ * function need to be excuted with one(same) connection
+ * will be sequential DAO function inside of f
+ * DAO function inside of f must use their last parameter as t to work with one(same) connection.
+ * 
+ * @param {object} task 
+ * optional.
+ * @returns return value of f
+ */
+const compressIntoTask = async (f, task = db, as) => {
+    return task.task(async t => await f(t)).then(data => data);
+}
+/**
+ * 
+ * @param {function} f 
+ * function need to be excuted with one(same) connection
+ * will be sequential DAO function inside of f
+ * DAO function inside of f must use their last parameter as t to work with one(same) connection.
+ * 
+ * @param {object} task 
+ * optional.
+ * @returns return value of f
+ */
+
+const compressIntoTx = async (f, task = db) => {
+    return task.tx(async t => await f(t)).then(data => data);
+}
+
+module.exports = { UserDAO, SocialDAO, ChannelDAO, compressIntoTask, compressIntoTx };
 
 /*
 다른 코드에 접근하려면 require('~~/DAO');만 하면 됨.

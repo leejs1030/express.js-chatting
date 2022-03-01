d2c36e73d581a03029fcc5bd969fe127d6172d1a
leejs1030 <leejs1030@korea.ac.kr>
2022-03-01 22:29:24 +0900

    Update controller/ctrl.js
    - Apply TypeScript

--- src/main/controller/ctrl.ts
+++ src/main/controller/ctrl.ts
@@ -1,20 +1,23 @@
 import { SocialDAO } from '../DAO';
+import { NextFunction, Request, Response } from 'express';
 
 //GET /
-const indexPage = async (req, res, next) =>{ //초기 화면
-    try{
-        const {user} = req.session;
+//초기 화면
+async function indexPage(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const { user } = req.session;
         let counts = 0;
-        if(user){ // 로그인 한 상태라면
+        if (user) { // 로그인 한 상태라면
             counts = (await SocialDAO.getReceivedById(user.id)).length; //요청받은 숫자
         }
-        return res.status(200).render('index.pug', {user, counts,
+        return res.status(200).render('index.pug', {
+            user, counts,
             csrfToken: req.csrfToken(),
         }); // 페이지 렌더링 응답
-    }catch(err){
+    } catch (err) {
         return next(err);
     }
-};
+}
 
 
 export {

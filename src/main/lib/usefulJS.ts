51bb211946834c25e8325c0d36a89d9cf06c9938
leejs1030 <leejs1030@korea.ac.kr>
2022-03-01 15:18:33 +0900

    Update usefulJS to use typeAssertion

--- src/main/lib/usefulJS.ts
+++ src/main/lib/usefulJS.ts
@@ -1,10 +1,10 @@
-const getAlertScript = (msg) => `<script>alert("${msg}");history.back();</script>`; // alert 메시지를 띄우기 위함
-const errorAt = (name, err) => { // 반복 작업 축소
+const getAlertScript = (msg: string) => `<script>alert("${msg}");history.back();</script>`; // alert 메시지를 띄우기 위함
+const errorAt = (name: string, err: Error) => { // 반복 작업 축소
     console.error("Error at " + name);
     console.error(err);
     return err;
 };
-const isNumber = (str) =>{ // 숫자인지 판별
+const isNumber = (str: string) =>{ // 숫자인지 판별
 	let x = parseInt(str);
 	if(isNaN(x)) return false;
 	return true;

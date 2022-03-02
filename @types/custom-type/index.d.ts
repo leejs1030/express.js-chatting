b770988bf2481f51d29b1ddefbacfe7cc20e56ea
leejs1030 <leejs1030@korea.ac.kr>
2022-03-03 00:58:51 +0900

    Refact: Add atomic-task to custom-type
    - It was too long to type
    pgPromise.IDatabase<{}, pg.IClient> | pgPromise.ITask<{}>
    to all task parameters. And it also required two lines of import code.

--- @types/custom-type/index.d.ts
+++ @types/custom-type/index.d.ts
@@ -32,4 +32,8 @@ declare module 'custom-type'{
         id: string,
         send_enter: boolean,
     }
+
+    import pgPromise from 'pg-promise';
+    import pg from 'pg-promise/typescript/pg-subset';
+    export type atomictask = (pgPromise.IDatabase<{}, pg.IClient> | pgPromise.ITask<{}>);
 }

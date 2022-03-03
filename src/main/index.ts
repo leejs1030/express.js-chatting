b0a034785ee935e0778ab404720afc7fbce8346b
leejs1030 <leejs1030@korea.ac.kr>
2022-03-03 16:24:51 +0900

    Update index.ts
    - Before, io.use(sharedsession(session)) caused error.
    - Parameter for io.use() is (socket.Socket<~~>, next) => void
    - Return type of sharedsession(session) is (socket.Socket, next) => void
    - Although they can be matched, it caused error. The only way to resolve
    it was using "as any".
    - This was because of version difference between "socket.io" from my
    node_modules and "socket.io" from express-socket.io-session node_modules
    - So, it can be resolved by following two ways.
    - First way: delete node_modules folder from
    node_modules/@types/express-socket.io-session/
    - Second way: create a new index.d.ts file in my @types folder and
    change path of express-socket.io-session by editing tsconfig.json
    - Althoug first way is more convenience, I selected second way.
    The reason is simple. node_modules folder is in .gitignore file.
    But tsconfig.json and @types folder is not in .gitignore.
    - So, I create new file "@types/my-shared-session/index.d.ts" and change
    path of "express-socket.io-session" from config.json to my new file.

--- src/main/index.ts
+++ src/main/index.ts
@@ -12,10 +12,7 @@ const channelPos = 2; // URI에서 "channels"가 등장하는 위치.
 const isChannelURI = (URI: string[]) => (URI[channelPos] === 'channels'); // 채널 URI인지 확인하기.
 
 // socket - express 간 세션 공유
-io.use(
-	(socket: socket.Socket, next: ((err?: any) => void)) =>
-		sharedsession(sessionmiddleware(SESSION_SECRET as string, PROTOCOL as string))(socket as any, next)
-);
+io.use(sharedsession(sessionmiddleware(SESSION_SECRET as string, PROTOCOL as string)));
 
 io.on('connection', async (socket: socket.Socket) => {
 	// 주소를 /단위로 끊어서 리스트로 저장. '' 문자열은 자동으로 제거됨.
